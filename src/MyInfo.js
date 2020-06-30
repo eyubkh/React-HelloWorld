dfd9e0a6ffd9ae60040c02d408e081c6cc7ab4ea
eyubkh <eyub.kh@gmail.com>
2020-06-30 20:12:36 +0200

    tercer push

---
+++ src/MyInfo.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default MyInfo;
+
+function MyInfo(){
+    return (
+        <div className="cont">
+            <h1>Ak</h1>
+            <p>Folleti desde que nacio, 23cm klk</p>
+            <ul>
+              <li>Cordoba</li>
+              <li>World trabels</li>
+              <li>Morroco</li>
+            </ul>
+        </div>
+    )
+  };
--- src/index.js
+++ src/index.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <h1>Hello World!</h1>,
-  document.getElementById('root')
-);
+import MyInfo from './MyInfo.js';
+
+
+
+ReactDOM.render(<MyInfo/>, document.querySelector('#root'))
+
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
